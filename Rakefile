11c05c7fd0e83356d80de62d1c219ddb3d48bddc
Ethan Miller <ethan.w.miller@gmail.com>
2016-03-13 22:43:31 -0400

    solution completed

--- Rakefile
+++ Rakefile
@@ -1,3 +1,8 @@
 task :default do
 	ruby "lib/part_one.rb"
 end
+
+task :clean do
+	rm "solution.txt"
+	touch "solution.txt"
+end
--- lib/part_one.rb
+++ lib/part_one.rb
@@ -14,6 +14,8 @@
 # the simtime and array of jobs will then be passed to fcfs and rr for the actual
 # scheduling.
 
+#$$$$ = currJobs.push(currJobs.shift) to reorder for RR!!!!!!
+
 require_relative 'job.rb'
 
 class PartOne
@@ -22,7 +24,7 @@ class PartOne
   end
 
   # perform operations on currently queued jobs
-  def timeStep(jobQueue)
+  def timeStepFCFS(jobQueue)
     if(jobQueue.empty?)
       return
     else
@@ -34,7 +36,20 @@ class PartOne
     end
   end
 
-  # process an individual trace file
+  def timeStepRR(jobQueue)
+    if(jobQueue.empty?)
+      return
+    else
+      jobQueue[0].increment
+      (1..((jobQueue.length)-1)).each do |i|
+        jobQueue[i].wait
+      end
+      # Here's the money, right here
+      jobQueue.push(jobQueue.shift)
+    end
+  end
+
+  # FCFS algorithm
   def fcfs(traceFile, outputFile)
     puts("Analyzing: #{traceFile} in FCFS")
 
@@ -75,7 +90,7 @@ class PartOne
       end
 
       # do operations on the current job queue
-      timeStep(currJobs)
+      timeStepFCFS(currJobs)
       timer+=1
     end
 
@@ -95,7 +110,60 @@ class PartOne
 
   def rr(traceFile, outputFile)
     puts("Analyzing #{traceFile} in RR")
-  end
+
+      # read lines of trace file as an array
+      traceArr = IO.readlines(traceFile)
+
+      # assign important variables: no. of jobs, simulation time, maximum length
+      totalJobs = traceArr[0].to_i
+      simTime = traceArr[1].to_i
+      maxJobLength = traceArr[2].to_i
+
+      # set variables for simulation
+      timer = 0
+      allJobs = Array.new
+      currJobs = Array.new
+      finishedJobs = Array.new
+      totalWaitTime = 0
+
+      # for each line in the trace file that describes a job, create job object
+      (3..((traceArr.length)-1)).each do |i|
+        str = traceArr[i].split(" ")
+        allJobs.push(Job.new(str[0], str[1]))
+      end
+
+      jobIterator = 0
+      while(timer <= simTime) do
+        unless allJobs.empty?
+          nextTime = allJobs[0].getStartTime
+          if(nextTime == timer)
+            currJobs.push(allJobs.shift)
+          end
+        end # end unless
+
+        unless currJobs.empty?
+          if(currJobs[0].getTimeLeft == 0)
+            finishedJobs.push(currJobs.shift)
+          end
+        end # end unless
+
+        timeStepRR(currJobs)
+        timer+=1
+      end # end while
+
+      finishedJobs.each do |job|
+        totalWaitTime += job.getWaitTime
+      end
+
+      puts("Finished jobs from #{traceFile}.")
+      puts("Total wait time: #{totalWaitTime}")
+      puts("Total jobs: #{totalJobs}")
+      averageWait = totalWaitTime.to_f/totalJobs.to_f
+      puts("Average wait time: #{averageWait}")
+
+      formattedFile = traceFile.split("/").pop.split(".").shift
+      outputFile.puts("RR #{formattedFile}: #{averageWait}")
+  end # end rr
 
   def main
     File.open("solution.txt", "w") do |outputFile|
